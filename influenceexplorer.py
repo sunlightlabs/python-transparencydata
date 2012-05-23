c05cea8d06a7dfa5d593eee0b9b480e68750d4dd
Alison Rowland <arowland@sunlightfoundation.com>
2012-05-23 13:43:05 -0400

    [#1073] Adding top industries by party donation method

--- influenceexplorer.py
+++ influenceexplorer.py
@@ -203,6 +203,14 @@ class Entities(SubAPI):
         """ Return top industries by lobbying spending. """
         return self._get_url_json('aggregates/industries/lobbying_clients/top_{0}.json'.format(limit), cycle)
 
+    def top_n_industry_donors_to_democrats(self, cycle=DEFAULT_CYCLE, limit=DEFAULT_LIMIT):
+        """ Return top industries by dollars donated to democrats. """
+        return self._get_url_json('aggregates/industries/party/D/top_{0}.json'.format(limit), cycle)
+
+    def top_n_industry_donors_to_republicans(self, cycle=DEFAULT_CYCLE, limit=DEFAULT_LIMIT):
+        """ Return top industries by dollars donated to republicans. """
+        return self._get_url_json('aggregates/industries/party/R/top_{0}.json'.format(limit), cycle)
+
     def top_n_pacs_by_indexp(self, cycle=DEFAULT_CYCLE, limit=DEFAULT_LIMIT):
         """ Return top PACs by independent expenditures. """
         return self._get_url_json('aggregates/orgs/indexp/top_{0}.json'.format(limit))
